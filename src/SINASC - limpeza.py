5e713539de23238a5651a2e6ef978da08d562a20
BuildTools <unconfigured@null.spigotmc.org>
2025-04-25 14:07:30 -0300

    #Diogo Update SINASC Limpeza

--- src/SINASC - limpeza.py
+++ src/SINASC - limpeza.py
@@ -3,10 +3,9 @@ import pandas as pd
 try:
     df = pd.read_csv("../datasets/nascidos_vivos.csv", encoding='latin1')
     print("Dataset nascidos vivos encontrado com sucesso!")
+    # Filtrando colunas e eliminando duplicados
+    df.drop_duplicates(inplace=True)
+    df = df[['CONSPRENAT', 'CODUFNATU']] 
+    df.to_csv('nascidos_vivos_limpo.csv')
 except FileNotFoundError:
     print("Dataset nascidos vivos não encontrado!")
-
-# Filtrando colunas e eliminando duplicados
-df.drop_duplicates(inplace=True)
-df = df[['CONSPRENAT', 'CODUFNATU']] 
-df.to_csv('nascidos_vivos_limpo.csv')
